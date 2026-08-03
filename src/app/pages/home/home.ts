2cbf343512b41883bde5482854160c6460a0a2e6
Vidóczi Péter <>
2026-08-03 10:03:11 +0200

    new template

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,15 +1,39 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { IdsButtonComponent } from '@i-cell/ids-angular/button';
+import {
+  IdsCardBodyDirective,
+  IdsCardComponent,
+  IdsCardHeaderComponent,
+  IdsCardSubtitleDirective,
+  IdsCardTitleDirective,
+} from '@i-cell/ids-angular/card';
+import { IdsCheckboxComponent } from '@i-cell/ids-angular/checkbox';
+import { IdsDividerComponent } from '@i-cell/ids-angular/divider';
+import { IdsRadioComponent, IdsRadioGroupComponent } from '@i-cell/ids-angular/radio';
+import { IdsSwitchComponent } from '@i-cell/ids-angular/switch';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [IdsButtonComponent],
+  imports: [
+    FormsModule,
+    IdsButtonComponent,
+    IdsCardBodyDirective,
+    IdsCardComponent,
+    IdsCardHeaderComponent,
+    IdsCardSubtitleDirective,
+    IdsCardTitleDirective,
+    IdsCheckboxComponent,
+    IdsDividerComponent,
+    IdsRadioComponent,
+    IdsRadioGroupComponent,
+    IdsSwitchComponent,
+  ],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  protected handleClick(): void {
-    console.log('Button clicked!');
-  }
+  protected notificationsEnabled = true;
+  protected selectedPlan = 'standard';
 }
